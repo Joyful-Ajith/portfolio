3b35a7425ff0fe55326ead25e187a2cd16397236
Ajith-website-ux <ajithaji1037@gmail.com>
2024-12-19 10:40:19 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: '/src/assets/Healthcare Landing Concept.jpeg',
+    title: "Health First",
+    desc: "",
+    link: "https://health-first-xi.vercel.app/",
+    git: "https://github.com/Ajith-website-ux/HealthFirst",
+  },
+  {
+    id: 2,
+    img: '/src/assets/Browse thousands of Rent Web images for design inspiration.jpeg',
+    title: "Ride ON",
+    link: "https://ride-on-ten.vercel.app/",
+    git: "https://github.com/Ajith-website-ux/Ride_ON-WebPage",
+  },
+  {
+    id: 3,
+    img: '/src/assets/Landing Page - Movie Ticket Booking.jpeg',
+    title: "Movie Ticket",
+    desc: "",
+    link: "https://ticket-booking-one-phi.vercel.app/",
+    git: "https://github.com/Ajith-website-ux/Ticket-Booking",
+  }
+  
+];
